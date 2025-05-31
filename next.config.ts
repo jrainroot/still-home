8b8c9d9a128ec7b9bf5be025059a64e2b821d371
jrainroot <jrainroot@gmail.com>
2025-05-31 20:28:04 +0900

    fix 2번씩 랜더링 되는 현상 false 처리

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // 콘솔 2번씩 찍혀서 false로 설정
+  reactStrictMode: false,
 };
 
 export default nextConfig;
